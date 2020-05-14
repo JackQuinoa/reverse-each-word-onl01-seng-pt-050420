1391606aa3e0d89d7c4bbe03e64abdf5e16f71a9
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 23:46:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,5 @@
 def reverse_each_word(sentence)
   sentence_array =[]
   sentence_array << sentence.split
-  sentence_array.each{|word| word.reverse!}.join
-    
-  end
-  
-    
-    
-  
-  
+  sentence_array.each{|word| word.reverse!}.join(" ")
 end
